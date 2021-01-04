292fa3924c883d9545753369c09d197f35bffed0
Chris Bailey <chris@ghostly.io>
2021-01-04 10:53:20 -0500

    feat(Entry): auth policy for entries
    allow only owner to edit and view their entries

---
+++ app/Policies/EntryPolicy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Entry;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class EntryPolicy {
+    use HandlesAuthorization;
+
+    public function view(User $user, Entry $entry): bool {
+        return $user->id === $entry->user_id;
+    }
+
+    public function update(User $user, Entry $entry): bool {
+        return $user->id === $entry->user_id;
+    }
+
+    public function delete(User $user, Entry $entry): bool {
+        return $user->id === $entry->user_id;
+    }
+
+    public function restore(User $user, Entry $entry): bool {
+        return $user->id === $entry->user_id;
+    }
+
+    public function foreDelete(User $user, Entry $entry): bool {
+        return $user->id === $entry->user_id;
+    }
+}
