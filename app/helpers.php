53ad51168e5ea096b6a78d8ef4447eb312e99e87
Chris Bailey <chris@ghostly.io>
2021-01-17 11:40:18 -0500

    :sparkles: (EntryStats,Helpers,EntryTable) color helper, table labels
    helper function to graby category colors, implement in entry table and entry stats.

--- app/helpers.php
+++ app/helpers.php
@@ -2,75 +2,94 @@
 
 use App\Models\User;
 
-if (!function_exists('cents_to_dollars')) {
+if (!function_exists("cents_to_dollars")) {
   function cents_to_dollars($cents) {
-    return number_format($cents / 100, 2, '.', '');
+    return number_format($cents / 100, 2, ".", "");
   }
 }
 
-if (!function_exists('dollars_to_cents')) {
+if (!function_exists("dollars_to_cents")) {
   function dollars_to_cents($dollars) {
-    return (int) ((float) preg_replace('/[^0-9.]/', '', $dollars) * 100);
+    return (int) ((float) preg_replace("/[^0-9.]/", "", $dollars) * 100);
   }
 }
 
-if (!function_exists('current_user')) {
+if (!function_exists("current_user")) {
   function current_user() {
     return User::find(auth()->user()->id);
   }
 }
 
-if (!function_exists('category_totals')) {
+if (!function_exists("category_totals")) {
   function category_totals($entries) {
-    return $entries->groupBy('category')->map(function ($item, $key) {
-      if ($key == 'income') {
+    return $entries->groupBy("category")->map(function ($item, $key) {
+      if ($key == "income") {
         return null;
       } else {
-        return $key = $item->sum('amount');
+        return $key = $item->sum("amount");
       }
     }, []);
   }
 }
 
-if (!function_exists('stat_bar_width')) {
+if (!function_exists("stat_bar_width")) {
   function stat_bar_width($amount, $total = 0) {
     $percent = $total / $amount;
     switch (true) {
       case $percent <= 16:
-        return 'w-1/6';
+        return "w-1/6";
         break;
       case $percent <= 20:
-        return 'w-1/5';
+        return "w-1/5";
         break;
       case $percent <= 25:
-        return 'w-1/4';
+        return "w-1/4";
         break;
       case $percent <= 33:
-        return 'w-1/3';
+        return "w-1/3";
         break;
       case $percent <= 40:
-        return 'w-2/5';
+        return "w-2/5";
         break;
       case $percent <= 50:
-        return 'w-1/2';
+        return "w-1/2";
         break;
       case $percent <= 60:
-        return 'w-3/5';
+        return "w-3/5";
         break;
       case $percent <= 66:
-        return 'w-2/3';
+        return "w-2/3";
         break;
       case $percent <= 75:
-        return 'w-3/4';
+        return "w-3/4";
         break;
       case $percent <= 80:
-        return 'w-4/5';
+        return "w-4/5";
         break;
       case $percent <= 85:
-        return 'w-5/6';
+        return "w-5/6";
         break;
       default:
-        return 'w-full';
+        return "w-full";
+        break;
+    }
+  }
+}
+
+if (!function_exists("get_category_color")) {
+  function get_category_color($category) {
+    switch ($category) {
+      case "need":
+        return "yellow-400";
+        break;
+      case "want":
+        return "red-400";
+        break;
+      case "savings":
+        return "blue-400";
+        break;
+      default:
+        return "green-400";
         break;
     }
   }
