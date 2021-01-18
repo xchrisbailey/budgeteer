d01c727b9603d2d15c87b0302fa81bc26076fc09
Chris Bailey <chris@ghostly.io>
2021-01-17 23:24:34 -0500

    :bug: (tailwind.config.js) safelist bg colors for categories

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ module.exports = {
   purge: {
     content: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
     options: {
-      safelist: ['w-1/6', 'w-1/5', 'w-1/4', 'w-1/3', 'w-2/5', 'w-1/2', 'w-3/5', 'w-2/3', 'w-3/4', 'w-4/5', 'w-5/6'],
+      safelist: [
+        'w-1/6',
+        'w-1/5',
+        'w-1/4',
+        'w-1/3',
+        'w-2/5',
+        'w-1/2',
+        'w-3/5',
+        'w-2/3',
+        'w-3/4',
+        'w-4/5',
+        'w-5/6',
+        'bg-green-400',
+        'bg-red-400',
+        'bg-yellow-400',
+        'bg-blue-400',
+      ],
     },
   },
 
